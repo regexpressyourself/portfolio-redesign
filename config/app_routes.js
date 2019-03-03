3faf4e2df2a83d00f4abebb922c4110b93a71c16
Sam <samuel.messina@siu.edu>
2019-03-03 11:28:38 -0600

    update resume stuff

--- config/app_routes.js
+++ config/app_routes.js
@@ -43,8 +43,22 @@ module.exports = (app) => {
   app.get('/oops', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/mail-error.html'));
   });
+  app.get('/resume.json', (req, res) => {
+    const resume = require('../public/static/documents/resume.json')
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(JSON.parse(JSON.stringify(resume)), null, 4));
+  });
+  app.get('/resume.pdf', (req, res) => {
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename=SamMessinaResume.pdf');
+    res.sendFile(path.join(__dirname, '../public/static/documents/SamMessinaResume.pdf'));
+  });
+  app.get('/resume', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/static/documents/resume.html'));
+  });
 
   app.use('/', express.static('public'));
+  app.use('/docs', express.static('public/static/documents'));
   app.use('/images', express.static('public/static/images'));
   app.use('/css', express.static('public/static/css'));
   app.use('/js', express.static('public/static/js'));
